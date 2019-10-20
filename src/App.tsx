2148c8385c66d6c9da2fc11cde2ecb75f33ec7d6
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-10-21 02:26:11 +0900

    wip issue #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,166 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// TODO 下記のデータをLeftSidebar,RightSidebarに入れて動的にしていく
-
-const templateData = {
-  templateName: '年賀はがき',
-  image: null,
-  pageSize: {
-    width: 100,
-    height: 148,
-  },
-  fontName: 'NotoSansCJKjp',
-  // これはまだアイデアだが、fontUriを持ってもいいかもしれない
-  // "fontUri": "https://labelmake3.firebaseapp.com/pdfmake/vfs_fonts.js"
-  fields: [
-    {
-      id: 'uuid-hogehoge1234',
-      fieldName: '[お届け先]郵便番号',
-      sampleData: '1234567',
-      position: {
-        x: 45,
-        y: 10,
-      },
-      size: {
-        width: 50,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'left',
-      fontSize: 22.9,
-      characterSpacing: 7.4,
-      lineHeight: 1,
-    },
-    {
-      id: 'uuid-hogehoge1235',
-      fieldName: '[お届け先]住所1',
-      sampleData: '東京都東京1|2|3|4',
-      position: {
-        x: 90,
-        y: 30,
-      },
-      size: {
-        width: 0,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'center',
-      fontSize: 9.2,
-      characterSpacing: 0,
-      lineHeight: 1,
-    },
-    {
-      id: 'uuid-hogehoge1236',
-      fieldName: '[お届け先]住所2',
-      sampleData: '東京マンション123号',
-      position: {
-        x: 83,
-        y: 35,
-      },
-      size: {
-        width: 0,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'center',
-      fontSize: 9.2,
-      characterSpacing: 0,
-      lineHeight: 1,
-    },
-    {
-      id: 'uuid-hogehoge1237',
-      fieldName: '[お届け先]氏名',
-      sampleData: '東京都 出得太 様',
-      position: {
-        x: 50,
-        y: 25,
-      },
-      size: {
-        width: 0,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'left',
-      fontSize: 20,
-      characterSpacing: 0,
-      lineHeight: 1,
-    },
-    {
-      id: 'uuid-hogehoge1238',
-      fieldName: '[差出人]郵便番号',
-      sampleData: '7654321',
-      position: {
-        x: 6.3,
-        y: 121.5,
-      },
-      size: {
-        width: 50,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'left',
-      fontSize: 14.8,
-      characterSpacing: 3.6,
-      lineHeight: 0,
-    },
-    {
-      id: 'uuid-hogehoge1239',
-      fieldName: '[差出人]住所1',
-      sampleData: '大阪府大阪1|2|3|4',
-      position: {
-        x: 29,
-        y: 50,
-      },
-      size: {
-        width: 0,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'center',
-      fontSize: 7,
-      characterSpacing: 0,
-      lineHeight: 1,
-    },
-    {
-      id: 'uuid-hogehoge1220',
-      fieldName: '[差出人]住所2',
-      sampleData: '大阪マンション123号',
-      position: {
-        x: 25,
-        y: 60,
-      },
-      size: {
-        width: 0,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'center',
-      fontSize: 7,
-      characterSpacing: 0,
-      lineHeight: 1,
-    },
-    {
-      id: 'uuid-hogehoge1221',
-      fieldName: '[差出人]氏名',
-      sampleData: '大阪府 出得太',
-      position: {
-        x: 12,
-        y: 57,
-      },
-      size: {
-        width: 0,
-        height: 100,
-      },
-      type: 'text',
-      alignment: 'left',
-      fontSize: 15,
-      characterSpacing: 0,
-      lineHeight: 1,
-    },
-  ],
-};
-
 const sidebarStyle = (option: any) => ({
   ...option,
   position: 'absolute',
@@ -173,7 +13,7 @@ const sidebarStyle = (option: any) => ({
 const inputStyle = () => ({
   border: '1px solid rgb(0, 0, 0)',
   marginBottom: 1,
-  color: '#fff',
+  color: '#000',
 });
 
 const miniInputStyle = () => ({ maxWidth: 50, ...inputStyle() });
@@ -202,10 +42,23 @@ const PanelWithAction = ({ head, action, children }: any) => (
   </div>
 );
 
-const LeftSidebar = () => (
+const LeftSidebar = ({
+  onChangeTemplate,
+  templateName,
+  image,
+  pageSize,
+  fontName,
+}: any) => (
   <div style={sidebarStyle({ left: 0 })}>
     <Panel head="Template Name">
-      <input style={inputStyle()} />
+      <input
+        style={inputStyle()}
+        value={templateName}
+        onChange={e => {
+          const value = e.target.value;
+          onChangeTemplate(value, 'templateName');
+        }}
+      />
     </Panel>
     <Panel head="Size">
       <div>
@@ -321,19 +174,30 @@ const RightSidebar = () => (
 interface Props {
   unityInstance: any;
   templateData: any;
+  onChangeTemplate: any;
 }
 interface State {}
-class App extends React.Component<Props, State> {
+class App extends Component<Props, State> {
   componentDidMount() {
-    console.log(this.props.unityInstance);
+    console.log(this.props.unityInstance, 'componentDidMount');
   }
   componentDidUpdate() {
-    console.log(this.props.templateData);
+    console.log(this.props.templateData, 'componentDidUpdate');
   }
   render() {
+    const { onChangeTemplate, templateData } = this.props;
+    const { fields, templateName, image, pageSize, fontName } = templateData;
     return (
       <>
-        <LeftSidebar />
+        <LeftSidebar
+          templateName={templateName}
+          image={image}
+          pageSize={pageSize}
+          fontName={fontName}
+          onChangeTemplate={onChangeTemplate}
+        />
+        {/* MEMO: 下記の要素にイベントが取られて正常にイベントハンドラできない */}
+        {/* FIXME: onChangeTemplateを動かす */}
         <div
           id="gameContainer"
           style={{
--- src/index.tsx
+++ src/index.tsx
@@ -10,41 +10,52 @@ const unityInstance = window.globalThis.UnityLoader.instantiate(
   { onProgress: window.globalThis.UnityProgress }
 );
 
-let templateData = {};
+const getInitialTemplateData = () => ({
+  templateName: 'test',
+  image: null,
+  pageSize: {
+    width: 210,
+    height: 297,
+  },
+  fontName: 'NotoSansCJKjp',
+  fields: [],
+});
 
-// TODO
-// Appコンポーネントに "unityInstance.SendMessage" するonChangeメソッドを渡して
-// Unityにデータの変更をリクエストする
+let templateData = getInitialTemplateData();
 
-ReactDOM.render(
-  <App unityInstance={unityInstance} templateData={templateData}></App>,
-  document.getElementById('root')
-);
+const onChangeTemplate = (value: any, key: string) => {
+  let data = {};
+  if (key === 'template') {
+    data = value;
+  } else {
+    data = Object.assign(value, { [key]: value });
+  }
+  console.log('--------------toUnity----------------');
+  console.log(data);
+  console.log('--------------toUnity----------------');
+  unityInstance.SendMessage('Canvas', 'ChangeTemplate', JSON.stringify(data));
+};
 
 window.WebInteraction = {
   onInit() {
-    unityInstance.SendMessage(
-      'Canvas',
-      'ChangeTemplate',
-      JSON.stringify({
-        templateName: '',
-        image: null,
-        pageSize: {
-          width: 210,
-          height: 297,
-        },
-        fontName: 'NotoSansCJKjp',
-        fields: [],
-      })
-    );
+    onChangeTemplate(getInitialTemplateData(), 'template');
   },
   onChangeTemplate: json => {
-    console.log('=============================');
+    console.log('=============fromUnity================');
     console.log(json);
-    console.log('=============================');
-    templateData = json;
+    console.log('=============fromUnity================');
+    templateData = JSON.parse(json);
   },
 };
+
+ReactDOM.render(
+  <App
+    unityInstance={unityInstance}
+    templateData={templateData}
+    onChangeTemplate={onChangeTemplate}
+  ></App>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
