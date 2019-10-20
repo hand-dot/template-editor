3df8fdeddea01ceb40c2b6566076a3873f530f06
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-10-20 23:45:55 +0900

    minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,21 @@ import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
 const unityInstance = window.globalThis.UnityLoader.instantiate(
   'gameContainer',
   'Build/WebGL-Dist.json',
   { onProgress: window.globalThis.UnityProgress }
+);
+
+let templateData = {};
+
+// TODO
+// Appコンポーネントに "unityInstance.SendMessage" するonChangeメソッドを渡して
+// Unityにデータの変更をリクエストする
+
+ReactDOM.render(
+  <App unityInstance={unityInstance} templateData={templateData}></App>,
+  document.getElementById('root')
 );
 
 window.WebInteraction = {
@@ -34,8 +42,10 @@ window.WebInteraction = {
     console.log('=============================');
     console.log(json);
     console.log('=============================');
+    templateData = json;
   },
 };
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
