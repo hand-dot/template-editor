36af21cad8e7b23f610e52f3ba6f17af07f95103
azure06 <gabri06e@gmail.com>
2019-11-04 18:20:29 +0900

    add types

--- unity/Assets/Scripts/Canvas/CanvasAttributes.cs
+++ unity/Assets/Scripts/Canvas/CanvasAttributes.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public partial class CanvasBehaviours : MonoBehaviour
 {
     private Template activeTemplate;
     private Template ActiveTemplate
     {
-        get {
+        get
+        {
             float widthFactor = 210f / 2480;
             float heightFactor = 297f / 3508;
             RectTransform sheetTransform = (UnityEngine.RectTransform)gameObject.transform.Find("Sheet");
@@ -26,10 +29,19 @@ public partial class CanvasBehaviours : MonoBehaviour
             float widthFactor = 2480 / 210f;
             float heightFactor = 3508 / 297f;
             RectTransform sheetTransform = (UnityEngine.RectTransform)gameObject.transform.Find("Sheet");
+            //sheetTransform.GetComponent<Image>().sprite = value.image != null ? toImage(value.image) : null;
             sheetTransform.sizeDelta = new Vector2(value.size.width * widthFactor, value.size.height * heightFactor);
             Debug.Log("factor " + value.size.width * widthFactor);
             activeTemplate = value;
         }
+    }
+
+
+    private void toImage(string inputText)
+    {
+        byte[] bytesToEncode = Encoding.UTF8.GetBytes(inputText);
+        string encodedText = Convert.ToBase64String(bytesToEncode);
+        //return  e
     }
 
 }
@@ -41,7 +53,9 @@ class Template
     public string image = "";
     public string fontName = "NotoSansCJKjp";
     public PageSize size = new PageSize();
+    public List<BaseField> fields = new List<BaseField>();
 }
+
 
 [System.Serializable]
 class PageSize
@@ -50,3 +64,42 @@ class PageSize
     public int height = 297;
 }
 
+[System.Serializable]
+class BaseField
+{
+    public string id = "";
+    public string name = "";
+    public string sampleData = "";
+    public Position position = new Position();
+}
+
+[System.Serializable]
+class TextField : BaseField
+{
+    public string type = "text";
+    public TextStyle style = new TextStyle();
+}
+
+[System.Serializable]
+class ImageField : BaseField
+{
+    public string type = "image";
+    public object style;
+}
+
+
+[System.Serializable]
+class Position
+{
+    public float width;
+    public float height;
+}
+
+
+class TextStyle
+{
+    public string alignment;
+    public int fontSize;
+    public float characterSpacing;
+    public float lineHeight;
+}
