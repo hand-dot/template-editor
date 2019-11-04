121ce897dc3db961fded91a34f9d679f5a736537
azure06 <gabri06e@gmail.com>
2019-11-04 16:37:07 +0900

    minor fix

--- unity/Assets/Scripts/Canvas/CanvasAttributes.cs
+++ unity/Assets/Scripts/Canvas/CanvasAttributes.cs
@@ -12,13 +12,13 @@ public partial class CanvasBehaviours : MonoBehaviour
             float heightFactor = 297f / 3508;
             RectTransform sheetTransform = (UnityEngine.RectTransform)gameObject.transform.Find("Sheet");
             Template template = new Template();
-            template.templateName = activeTemplate.templateName;
+            template.name = activeTemplate.name;
             template.image = activeTemplate.image;
             template.fontName = activeTemplate.fontName;
             PageSize pageSize = new PageSize();
             pageSize.width = Convert.ToInt32(sheetTransform.rect.width * widthFactor);
             pageSize.height = Convert.ToInt32(sheetTransform.rect.height * heightFactor);
-            template.pageSize = pageSize;
+            template.size = pageSize;
             return template;
         }
         set
@@ -26,8 +26,8 @@ public partial class CanvasBehaviours : MonoBehaviour
             float widthFactor = 2480 / 210f;
             float heightFactor = 3508 / 297f;
             RectTransform sheetTransform = (UnityEngine.RectTransform)gameObject.transform.Find("Sheet");
-            sheetTransform.sizeDelta = new Vector2(value.pageSize.width * widthFactor, value.pageSize.height * heightFactor);
-            Debug.Log("factor " + value.pageSize.width * widthFactor);
+            sheetTransform.sizeDelta = new Vector2(value.size.width * widthFactor, value.size.height * heightFactor);
+            Debug.Log("factor " + value.size.width * widthFactor);
             activeTemplate = value;
         }
     }
@@ -37,10 +37,10 @@ public partial class CanvasBehaviours : MonoBehaviour
 [System.Serializable]
 class Template
 {
-    public string templateName = "";
+    public string name = "";
     public string image = "";
     public string fontName = "NotoSansCJKjp";
-    public PageSize pageSize = new PageSize();
+    public PageSize size = new PageSize();
 }
 
 [System.Serializable]
